50e4eb671e0d53a44f16b2efef660c4ee06e9d7f
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-07-19 18:12:02 +0200

    kraj srijede

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 const mime = require('mime');
 const models = require('./models/models');
-const { Book, Author }= require('./models/models');
+const { Book, User, Author }= require('./models/models');
+
 require('dotenv').config();
 
 // Konektuj seMongoDB
@@ -12,28 +13,29 @@ mongoose.connect('mongodb://localhost:27017/bookstore', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('Povezano sa MongoDB'))
+  .catch(err => console.error('Greška pri konektovanju na MongoDB:', err));
 
 
 // Podesi EJS
 app.set('view engine', 'ejs');
 
-// Setuj views direktorijum
+// Setuj direktorijum za views
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware je tu
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 app.use(express.static('assets', { "extensions": ["css"] }));
 
-// staticni fajlovi
+// Staticni fajlovi
 app.use('/assets', express.static(path.join(__dirname, 'assets'), {
   setHeaders: (res, filePath) => {
     res.setHeader('Content-Type', mime.getType(filePath));
   }
 }));
 
+// Prikazuje pocetnu stranicu
 app.get('/', async (req, res) => {
   try {
     let query = {};
@@ -48,19 +50,52 @@ app.get('/', async (req, res) => {
       };
     }
 
+    // Get the logged-in user's username
+    const username = req.user ? req.user.username : null;
+
     const users = await models.User.find();
     const books = await models.Book.find(query).exec();
-    res.render('index', { users, books });
+    res.render('index', { username, users, books });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Greska servera.');
+    res.status(500).send('Greška servera.');
   }
 });
 
+app.get('/dashboard', async (req, res) => {
+  try {
+    let query = {};
+    const searchQuery = req.query.search;
+    if (searchQuery) {
+      query = {
+        $or: [
+          { title: { $regex: searchQuery, $options: 'i' } },
+          { id: parseInt(searchQuery) || 0 },
+          { page_count: parseInt(searchQuery) || 0 }
+        ]
+      };
+    }
+
+    // Get the logged-in user's username
+    const username = req.user ? req.user.username : null;
+
+    const books = await models.Book.find(query).exec();
+    res.render('dashboard', { username, books });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+
+
+
+// Prikazuje formu za kreiranje novog korisnika
 app.get('/users/new', (req, res) => {
   res.render('new-user');
 });
 
+// Dodaje novog korisnika
 app.post('/users', async (req, res) => {
   try {
     const user = new models.User(req.body);
@@ -68,23 +103,107 @@ app.post('/users', async (req, res) => {
     res.redirect('/');
   } catch (err) {
     console.error(err);
-    res.status(500).send('Greska servera.');
+    res.status(500).send('Greška servera.');
   }
 });
 
+// Prikazuje formu za registraciju novog korisnika
+// Kreira novog korisnika
+app.post('/signup', async (req, res) => {
+  try {
+    const {
+      id,
+      user_type_id,
+      user_gender_id,
+      name,
+      JMBG,
+      email,
+      username,
+      email_verified_at,
+      password,
+      photo,
+      remember_token,
+      last_login_at,
+      login_count,
+      created_at,
+      updated_at,
+      github,
+      active
+    } = req.body;
+
+    const user = new User({
+      id,
+      user_type_id,
+      user_gender_id,
+      name,
+      JMBG,
+      email,
+      username,
+      email_verified_at,
+      password,
+      photo,
+      remember_token,
+      last_login_at,
+      login_count,
+      created_at,
+      updated_at,
+      github,
+      active: active === 'true' // Cast to boolean
+    });
+
+    await user.save();
+    res.redirect('/'); // Redirect to the desired page
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.render('login'); // Assuming you have a "login.ejs" file in your views directory
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+  try {
+    const { uname, psw } = req.body;
+
+    // vidi postoji i user u bazi
+    const user = await User.findOne({ username: uname });
+
+    if (user) {
+      // User vec postoji
+      if (user.password === psw) {
+        // Password se poklapa
+        const welcomeMessage = `Welcome, ${user.username}!`;
+        // 
+        return res.send(`<script>alert("${welcomeMessage}"); window.location.href="/dashboard";</script>`);
+      }
+    }
+
+    // nije se poklopilo
+    res.render('login', { error: 'Pogresan user ili password' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server greska');
+  }
+});
+
+
+// Prikazuje formu za uređivanje knjige
 app.get('/books/:id/edit', async (req, res) => {
   try {
     const book = await Book.findOne({ id: req.params.id }).populate('author');
     const authors = await Author.find({}, 'NameSurname');
 
     if (!book) {
-      return res.status(404).send('Book not found');
+      return res.status(404).send('Knjiga nije pronađena');
     }
 
     res.render('edit-book', { book, authors });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Greska servera');
+    res.status(500).send('Greška servera');
   }
 });
 
