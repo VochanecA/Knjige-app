45ba73acf7004497cbad73ecb5d323c7d07d43cd
VochanecA <126856875+VochanecA@users.noreply.github.com>
2023-08-04 17:54:12 +0200

    mali revers

--- assets/js/bookDetails.js
+++ assets/js/bookDetails.js
@@ -1,31 +1,16 @@
-// public/js/bookDetails.js
-
-document.addEventListener("DOMContentLoaded", () => {
-  const selectBook = document.getElementById("book");
-  if (selectBook) {
-    selectBook.addEventListener("change", updateBookDetails);
-  }
-});
-
-async function updateBookDetails() {
+function updateBookDetails() {
   const selectedBookId = document.getElementById("book").value;
-  try {
-    const response = await fetch(`/api/books/${selectedBookId}`);
-    const selectedBook = await response.json();
+  const selectedBook = booksData.find(book => book._id === selectedBookId);
+  console.log("Selected book:", selectedBook);
 
-    console.log("Selected book:", selectedBook);
-
-    if (selectedBook) {
-      console.log("ISBN:", selectedBook.ISBN);
-      document.getElementById("isbn").value = selectedBook.ISBN ? selectedBook.ISBN.toString() : "";
-      document.getElementById("author").value = selectedBook.authors;
-      // Add other fields update as needed
-    } else {
-      // Clear the fields if no book is selected
-      document.getElementById("isbn").value = "";
-      document.getElementById("author").value = "";
-    }
-  } catch (error) {
-    console.error("Error fetching book details:", error);
+  if (selectedBook) {
+    console.log("ISBN:", selectedBook.ISBN);
+    document.getElementById("isbn").value = selectedBook.ISBN ? selectedBook.ISBN.toString() : "";
+    document.getElementById("author").value = selectedBook.authors;
+    // Add other fields update as needed
+  } else {
+    // Clear the fields if no book is selected
+    document.getElementById("isbn").value = "";
+    document.getElementById("author").value = "";
   }
 }
